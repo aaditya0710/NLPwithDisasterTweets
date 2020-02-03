4937b0a7453355d92c2d41d9b672b46f91c6a88c
aaditya0710 <59140388+aaditya0710@users.noreply.github.com>
2020-02-03 10:29:50 +0530

    Update kaggle_competition.py

--- kaggle_competition.py
+++ kaggle_competition.py
@@ -8,22 +8,28 @@ Original file is located at
 """
 
 import pandas as pd
-import pandas as pd
+import numpy as np
+from sklearn import feature_extraction,linear_model,model_selection,preprocessing
+from sklearn.feature_extraction.text import TfidfVectorizer
+
 train = pd.read_csv("/content/train.csv")
 test = pd.read_csv("/content/test.csv")
 train.head()
+
 train.isnull().sum()
 train[train['target']==0]['text'].values[1]
 train[train['target']==1]['text'].values[1]
 x = train['text']
 y = train['target']
-from sklearn import feature_extraction,linear_model,model_selection,preprocessing
-from sklearn.feature_extraction.text import TfidfVectorizer
+
 vectorizer = TfidfVectorizer()
 xtrain_tfidf = vectorizer.fit_transform(x)
+
 from sklearn.svm import LinearSVC
 clf = LinearSVC()
 clf.fit(train_idf,y)
+
+
 from sklearn import model_selection
 scores = model_selection.cross_val_score(clf,train_idf,train['target'],cv=3,scoring='f1')
 scores
